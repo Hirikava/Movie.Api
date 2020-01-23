b7ab7efc2c973b342f607e7e79a6218ffa935c8b
Alexander <alexxdaas@gmail.com>
2020-01-24 01:20:15 +0500

    add sessions

---
+++ Movie.Api/MovieApi/Clients/Sessions/SessionsClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Movie.Api.Providers.DataBase;
+
+namespace Movie.Api.Clients.Sessions
+{
+    public class SessionsClient : ISessionsClient
+    {
+        private readonly IDataBaseConnectionProvider dataBaseConnectionProvider;
+
+        public SessionsClient(IDataBaseConnectionProvider dataBaseConnectionProvider)
+        {
+            this.dataBaseConnectionProvider = dataBaseConnectionProvider;
+        }
+
+        public async Task<Guid> CreateSessionOnUser(Guid userId)
+        {
+            using (var connection = dataBaseConnectionProvider.GetConnection())
+            {
+                connection.Open();
+                var newSessionId = Guid.NewGuid();
+                var command = new SqlCommand("",connection);
+                await command.ExecuteNonQueryAsync().ConfigureAwait(false);
+                return newSessionId;
+            }
+        }
+    }
+}
